a8f8c8e8243444fdbbe5c9f71a93cad329edaa5e
novalCandra <dinatanovel5@gmail.com>
2026-07-18 14:24:47 +0700

    feat : ✅ Set Up STYLE

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +15,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1 className='text-yellow font-fredoka'>Get started</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>

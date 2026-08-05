d8803a60ae6576b0ef689df66fc65baed157aa6d
novalCandra <dinatanovel5@gmail.com>
2026-08-05 14:13:58 +0700

    feat : ✅ Add Page Slicing (Page & Pomodoro)

--- src/page/auth/user/dashboard.tsx
+++ src/page/auth/user/dashboard.tsx
@@ -1,7 +1,27 @@
+import AsidSidebar from "../../../components/atoms/aside/Aside";
+import ButtonDarkMode from "../../../components/atoms/button/ButtonDarkMode";
+import ButtonModal from "../../../components/atoms/button/ButtonModal";
+import { motion } from "motion/react"
 export default function DashboardUser() {
   return (
-    <div>
-      <h1>HELLO</h1>
+    <div className="flex flex-1">
+      <AsidSidebar />
+      <div className="container flex flex-col bg-paper px-5 py-2 min-h-screen space-y-5 max-w-full">
+        <motion.div initial={{ opacity: 0, x: -100 }} animate={{ opacity: 1, x: 0 }} transition={{ delay: 0.4, duration: 0.3 }}>
+          <div className="flex flex-col space-y-5">
+            <span className="font-fredoka text-gray-500 font-semibold text-md md:text-2xl">Tuesday, October 24</span>
+            <h2 className="font-fredoka font-semibold text-4xl md:text-6xl">
+              Good Morning, Savira<span className="text-pink">.</span>
+            </h2>
+            <p className="font-fredoka text-gray-500 text-md md:text-xl">You Have <span className="font-semibold text-black">3 Things</span> to move forward today.</p>
+          </div>
+
+          <div className="flex flex-row space-x-3 mt-5">
+            <ButtonDarkMode>Dark</ButtonDarkMode>
+            <ButtonModal>Capture</ButtonModal>
+          </div>
+        </motion.div>
+      </div>
     </div>
   )
 }
---
+++ src/page/auth/user/pomodoro/pomodoro.tsx
@@ -0,0 +1,28 @@
+import AsidSidebar from "../../../../components/atoms/aside/Aside";
+import CardFocusFomodoro from "../../../../components/atoms/card/CardFocus";
+import CardPomoro from "../../../../components/atoms/card/CardPomodoro";
+import { motion } from "motion/react"
+const PomodoroPage = () => {
+  return (
+    <div className="flex min-h-screen overflow-hidden">
+      <AsidSidebar />
+      <div className="flex flex-1 flex-col h-screen bg-paper max-w-full px-4 py-2 space-y-6 md:py-10 md:px-0 lg:px-20 overflow-y-auto md:overflow-hidden lg:overflow-y-auto scrollbar scrollbar-thumb-black scrollbar-track-paper">
+        <motion.div initial={{opacity : 0, y : -100}} animate={{opacity : 1, y : 0}} transition={{delay : 0.2, duration : 0.3}} className="flex flex-col space-y-5 mx-auto">
+          {/* <div className=""> */}
+            <h2 className="text-5xl font-fredoka font-semibold text-dark-surface">
+              Procted Your attention.
+            </h2>
+            <p className="text-xl font-fredoka text-gray-600">
+              A Tiny timer for the next right thing.
+            </p>
+            <div className="flex flex-col md:flex-col lg:flex-row space-x-0 md:space-x-10 space-y-10 md:space-y-15 lg:space-y-0">
+              <CardPomoro />
+              <CardFocusFomodoro />
+            </div>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default PomodoroPage;

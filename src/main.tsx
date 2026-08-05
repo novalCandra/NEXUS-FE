ee7245c26b4ebf0346c124241c2581d851ace6da
novalCandra <dinatanovel5@gmail.com>
2026-08-05 14:10:21 +0700

    feat : ✅Add Dark Mode Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
-import Router from "./router/Router.js";
+import Router from './router/Router.js';
 import DarkModeContextProvider from './context/DarkMode.js';
 createRoot(document.getElementById('root')!).render(
-  <DarkModeContextProvider>
+  <DarkModeContextProvider >
     <RouterProvider router={Router} />
   </DarkModeContextProvider>
 )

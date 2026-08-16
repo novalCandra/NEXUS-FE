3083c5c644b9242c7311851905d469f57acd16f7
novalCandra <dinatanovel5@gmail.com>
2026-08-16 13:36:21 +0700

    feat : ⚒️ Repair Button Shows Password (Atoms)

--- src/components/atoms/button/ButtonShowPassowrd.tsx
+++ src/components/atoms/button/ButtonShowPassowrd.tsx
@@ -1,20 +1,16 @@
 import { Eye, EyeOff } from "lucide-react";
-import { useState } from "react";
 
 type TypeButtonShowPassword = {
     stylePosition: string
+    showPassword: boolean
+    onToggle: () => void
 }
 
 const ButtonShowPassword = (props: TypeButtonShowPassword) => {
-    const { stylePosition } = props
-    const [showPassword, setShowPassowrd] = useState<boolean>(false);
-
-    function tampilkanPassword() {
-        setShowPassowrd(prev => !prev)
-    }
+    const { stylePosition, showPassword, onToggle } = props
 
     return (
-        <button onClick={tampilkanPassword} className={`absolute px-2 ${stylePosition}`}>{showPassword ? <Eye className="size-5 text-gray-400 " /> : <EyeOff className="size-5 text-gray-400 " />}</button>
+        <button type="button" onClick={onToggle} className={`absolute px-2 ${stylePosition}`}>{showPassword ? <Eye className="size-5 text-gray-400 " /> : <EyeOff className="size-5 text-gray-400 " />}</button>
     )
 }
 

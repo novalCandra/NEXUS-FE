3fc690e16a55321da7141c1af6061b2c3ed97be7
novalCandra <dinatanovel5@gmail.com>
2026-08-27 09:30:05 +0700

    feat : ✅ Add Atoms (Card)

--- src/components/atoms/card/CardInfo.tsx
+++ src/components/atoms/card/CardInfo.tsx
@@ -13,8 +13,8 @@ type TypeInfoString = {
 const CardInformasi = (props: TypeInfoString) => {
     const { text, heading, icon: Icon, style } = props
     return (
-        <motion.div whileHover={{ scale: 1.05, transition: { duration: 0.2 } }} transition={{ duration: 0.4 }} className="bg-white w-91 md:w-72 lg:w-64 h-72 md:h-72 lg:h-96 flex flex-col border-2 px-4 py-5 space-y-9 md:space-y-5 border-black  rounded-md shadow-black-100 cursor-pointer justify-center relative">
-            <div className={`${style} px-3 py-4  w-12 rounded-md border-2 border-black`}>
+        <motion.div whileHover={{ scale: 1.02, transition: { duration: 0.2 } }} transition={{ duration: 0.4 }} className="bg-white w-85 md:w-72 lg:w-64 h-72 md:h-72 lg:h-96 flex flex-col border-2 px-4 py-5 space-y-9 md:space-y-5 border-black  rounded-md shadow-black-100 cursor-pointer justify-center relative">
+            <div className={`${style} px-3 py-4 w-12 rounded-md border-2 border-black`}>
                 {Icon && <Icon />}
             </div>
             <div className="flex flex-col space-y-2.5">
--- src/components/atoms/card/CardPomodoro.tsx
+++ src/components/atoms/card/CardPomodoro.tsx
@@ -1,20 +1,27 @@
 import { Clock } from "lucide-react";
+import { Activity } from "react";
+import ModalAddNumber from "../modal/ModalAddNumber";
+import { useHooksPomodoro } from "../../../hooks/useHooksPomodoro";
 
 const CardPomoro = () => {
+  const { timer, addTimer, formatTimer, handleAddTimer, setStart, start, setAddTimer } = useHooksPomodoro()
   return (
-    <div className="bg-white flex flex-col space-y-2.5 justify-between px-10 py-4 items-center w-96 md:w-170 lg:w-100 h-96 shadow-black-100 border-2 border-black">
+    <div className="bg-white flex flex-col space-y-2.5 justify-between px-10 py-4 items-center w-96 md:w-170 lg:w-120 h-80 md:h-96 shadow-black-100 border-2 border-black relative">
       <span className="text-base font-semibold font-fredoka text-black uppercase">
         deep work session
       </span>
-      <h2 className="text-9xl font-fredoka font-extrabold">25:00</h2>
+      <h2 className="text-7xl md:text-9xl lg:text-8xl font-fredoka font-extrabold">{formatTimer(timer)}</h2>
       <div className="flex flex-row justify-between gap-5">
-        <button className="bg-black text-white rounded-md w-64 py-5 font-fredoka font-bold">
-          Start
+        <button onClick={() => setStart(prev => !prev)} type="button" className="bg-black text-white rounded-md w-64 py-5 font-fredoka font-bold text-base md:text-xl lg:text-2xl cursor-pointer">
+          {start ? "pause" : "start"}
         </button>
-        <button className="bg-white border-2 border-black py-5 px-6 rounded-md">
+        <button type="button" onClick={handleAddTimer} className="bg-white border-2 border-black py-5 px-6 rounded-md cursor-pointer">
           <Clock size={20} />
         </button>
       </div>
+      <Activity mode={addTimer ? "visible" : "hidden"} >
+        <ModalAddNumber removeModal={() => setAddTimer(false)} />
+      </Activity>
     </div>
 
   );

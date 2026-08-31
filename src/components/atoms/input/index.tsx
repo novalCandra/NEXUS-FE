61711bcf72d2c904103497170ff100f9c92237d7
novalCandra <dinatanovel5@gmail.com>
2026-08-31 07:48:30 +0700

    feat : ⚒️ Repair Input (Atoms)

--- src/components/atoms/input/index.tsx
+++ src/components/atoms/input/index.tsx
@@ -7,7 +7,7 @@ type InputAtomsType = {
 const InputAtoms = (props: InputAtomsType) => {
     const { name, placeholder, type, styleCenter } = props
     return (
-        <input type={type} name={name} id={name} placeholder={placeholder} className={`ring-3 ring-black px-2 py-2 focus:outline-none focus:ring-2 focus:ring-pink text-black font-semibold placeholder:text-dark-surface/50 placeholder:font-bold placeholder:font-fredoka bg-paper  no-spinner ${styleCenter}`} required />
+        <input type={type} name={name} id={name} placeholder={placeholder} className={`ring-3 ring-black px-2 py-2 focus:outline-none focus:ring-2 focus:ring-pink text-black font-semibold placeholder:text-dark-surface/50 placeholder:font-bold placeholder:font-fredoka bg-paper  no-spinner ${styleCenter}`} />
     )
 }
 

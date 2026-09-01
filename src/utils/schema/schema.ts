2d66a9404c81b04e95eeae073842bbf4f7e2fd26
novalCandra <dinatanovel5@gmail.com>
2026-09-01 19:37:37 +0700

    feat : ⚒️ Repair Schema

--- src/utils/schema/schema.ts
+++ src/utils/schema/schema.ts
@@ -1,8 +1,8 @@
 import z from "zod"
 
 export const SchemaAuthRegister = z.object({
-    name: z.string().min(1, "required name").max(255, "maximal 255 karakter"),
-    email: z.string().min(1, "email wajib diisi").max(10, "maximal 255 karakter"),
+    name: z.string().min(1, "Wajib mengisi nama").max(255, "maximal 255 karakter"),
+    email: z.string().min(1, "email wajib diisi"),
     password: z.string().min(1, "password wajib diisi").max(10, "maximal 10 karakter")
 })
 

40885d9daf1b405f83a8beac66ad21bca4a2a39c
novalCandra <dinatanovel5@gmail.com>
2026-09-01 19:39:22 +0700

    feat : ✅ add Repair (Auth)

--- src/components/template/AuthTemplate.tsx
+++ src/components/template/AuthTemplate.tsx
@@ -1,4 +1,4 @@
-import { Shield, type LucideIcon } from "lucide-react";
+import { type LucideIcon } from "lucide-react";
 import { motion } from "motion/react";
 
 type TypeAuthTemplate = {
@@ -33,10 +33,10 @@ const AuthTemplate = (props: TypeAuthTemplate) => {
         iraOliv
       </h2>
       <div
-        className={`flex flex-col items-center bg-white ${tinggiCard} py-11 space-y-5 justify-between ring-2 ring-black shadow-black relative`}
+        className={`flex flex-col items-center bg-white ${tinggiCard} py-20 space-y-5 justify-between ring-2 ring-black shadow-black relative`}
       >
         <div className="flex flex-col justify-center items-center space-y-2 px-10 text-center">
-          <span className={`px-2 py-2  ${styleIcon}  ring-2 ring-black`}>
+          <span className={`px-2 py-2 ${styleIcon}  ring-2 ring-black`}>
             <Icon size={26} />
           </span>
           <h1 className="font-fredoka text-3xl text-center font-bold">
@@ -48,12 +48,12 @@ const AuthTemplate = (props: TypeAuthTemplate) => {
         </div>
         {children}
       </div>
-      <div className="flex flex-row space-x-1">
+      {/* <div className="flex flex-row space-x-1">
         <Shield className="text-gray-400" />
         <span className="font-fredoka text-gray-400">
           Your data is private by default.
         </span>
-      </div>
+      </div> */}
     </motion.div>
   );
 };

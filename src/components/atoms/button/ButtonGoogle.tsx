c771bc759183067a6d35b11614c736944adeaf51
novalCandra <dinatanovel5@gmail.com>
2026-09-01 19:39:50 +0700

    feat : ✅ add Repair && Google (Auth)

--- src/components/atoms/button/ButtonGoogle.tsx
+++ src/components/atoms/button/ButtonGoogle.tsx
@@ -2,19 +2,20 @@ import { FcGoogle } from "react-icons/fc";
 
 type TypesButtonGoogle = {
   children: string;
-  type: "submit" | "button" | "reset";
+  type?: "submit" | "button" | "reset";
+  href: string;
 };
 
 const ButtonGoogle = (props: TypesButtonGoogle) => {
-  const { type, children } = props;
+  const { children, href } = props;
   return (
-    <button
-      type={type}
+    <a
+      href={href}
       className={`flex flex-row items-center justify-center gap-2 px-2 py-3 bg-white text-pink font-fredoka font-semibold ring-2 ring-black shadow-black transition duration-200 delay-150 hover:shadow cursor-pointer`}
     >
       <FcGoogle size={23} />
       {children}
-    </button>
+    </a>
   );
 };
 

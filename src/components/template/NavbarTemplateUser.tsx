4f5216f2af37c9e4758483a7aea8889a2901a115
novalCandra <dinatanovel5@gmail.com>
2026-09-01 19:38:30 +0700

    feat : ✅ Add Link

--- src/components/template/NavbarTemplateUser.tsx
+++ src/components/template/NavbarTemplateUser.tsx
@@ -1,13 +1,17 @@
+import { Link } from "react-router";
 import ButtonProfile from "../atoms/button/ButtonProfile";
 import InputSearch from "../atoms/input/InputSearch";
 
 const NavbarTemplateUser = () => {
   return (
     <nav className="flex flex-row justify-between px-10 py-5 bg-paper border-b-4 border-black max-w-full">
-      <h2 className="flex flex-row items-center gap-1 text-2xl font-fredoka font-semibold">
-        <span className="px-2 py-1 rounded-md bg-pink">V</span>
-        iraOliv
-      </h2>
+      <Link to={"/"}>
+        <h2 className="flex flex-row items-center gap-1 text-2xl font-fredoka font-semibold">
+          <span className="px-2 py-1 rounded-md bg-pink">V</span>
+          iraOliv
+        </h2>
+      </Link>
+
       <InputSearch placeholder="Search Your Mind" type="text" TextCMD="K" />
       <ButtonProfile />
     </nav>

10431ccf9f6c35c4165140c6f58828f354d080de
novalCandra <dinatanovel5@gmail.com>
2026-09-15 11:18:29 +0700

    feat : ✅ add schema (profile)

--- src/utils/schema/schema.ts
+++ src/utils/schema/schema.ts
@@ -7,3 +7,9 @@ export const SchemaAuthRegister = z.object({
 })
 
 export const SchemaAuthLogin = SchemaAuthRegister.omit({ name: true })
+
+export const SchemaProfileUsers = z.object({
+    name: z.string().min(1, "Wajib mengisi nama").max(255, "maximal 255 karakter"),
+    email: z.string().min(1, "email wajib diisi"),
+    password: z.string().min(1, "password wajib diisi").max(10, "maximal 10 karakter")
+})
